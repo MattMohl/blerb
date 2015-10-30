8a40bd1004766d73191ee30d8c9ba500e94d4933
MattMohl <mattmohldev@gmail.com>
2015-10-29 22:00:34 -0400

    fuck this framework

--- dist/public/js/main.js
+++ dist/public/js/main.js
@@ -1,55 +1,62 @@
-// var ExampleComponent = React.createClass({
-// 	displayName: "ExampleComponent",
-// 	mixins: [ReactFireMixin],
-
-// 	render: function render() {
-// 		var createCard = function createCard(cardName, cardType) {
-// 			return React.createElement(
-// 				'li',
-// 				{ key: cardName + cardType },
-// 				cardName,
-// 				cardType
-// 			);
-// 		};
-// 		return React.createElement(
-// 			'ul',
-// 			null,
-// 			this.props.items.map(createCard)
-// 		);
-// 	}
-// });
-
-// var ExampleApp = React.createClass({
-// 	displayName: "ExampleApp",
-
-// 	render: function render() {
-// 		return React.createElement(
-// 			"div",
-// 			null,
-// 			"Hello"
-// 			);
-// 	}
-
-// });
-
-// ReactDOM.render(React.createElement(ExampleApp, null), mountNode);
-
-"use strict";
-
-var HelloMessage = React.createClass({
-  displayName: "HelloMessage",
-
-  render: function render() {
-    return React.createElement(
-      "div",
-      null,
-      "Hello ",
-      this.props.name
-    );
-  }
+var DraftList = React.createClass({displayName: 'DraftList',
+	render: function render() {
+		var createSet = function createSet(name, index) {
+			return React.createElement(
+				'li',
+				{key: index + name},
+				name
+				);
+		};
+		return React.createElement(
+			'ul',
+			null,
+			this.sets.map(createSet)
+			);
+	}
 });
 
-ReactDOM.render(React.createElement(HelloMessage, { name: "John" }), mountNode);
+var DraftApp = React.createClass({
+	mixins: [ReactFireMixin],
+	componentWillMount: function() {
+		this.firebaseRef = new Firebase('https://ReactFireTodoApp.firebaseio.com/items/');
+		this.firebaseRef.limitToLast(25).on('value', function(dataSnapshot) {
+			var items = [];
+			dataSnapshot.forEach(function(childSnapshot) {
+				var item = childSnapshot.val();
+				item['.key'] = childSnapshot.key();
+				items.push(item);
+			}.bind(this));
 
+			this.setState({
+				items: items
+			});
+		}.bind(this));
+		
+		// this.ref = new Firebase('https://mtgsetsmm.firebaseio.com/');
+		// this.ref.on('value', function(data) {
+		// 	var sets = [];
+		// 	data.forEach(function(childData) {
+		// 		sets.push(childData.val());
+		// 	});
+		// 	this.setState({
+		// 		sets: sets
+		// 	});
+		// });
+		// 	console.log(this);
+		// this.bindAsArray(this.ref, "items");
+		// console.log(this, this.state);
+	},
+	render: function render() {
+		// return React.createElement(DraftList, {items: this });
+	}
+});
 
-React.createElement('div',null, "Hello");
+ReactDOM.render(
+	React.createElement(DraftApp, null),
+	document.getElementById('container')
+	);
+
+// ReactDOM.render(
+// 	React.createElement(Hello, {name: 'World'}),
+// 	document.getElementById('container')
+// 	);
